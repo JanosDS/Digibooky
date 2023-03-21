0ddeaa3019492715e9dfc28aa447da8f12fd65fa
gulsumgucenmez <86490486+gulsumgucenmez@users.noreply.github.com>
2023-03-21 19:54:51 +0100

    package setup for user

---
+++ src/main/java/com/switchfully/digibooky/dto/UserMapper.java
@@ -0,0 +1,11 @@
+package com.switchfully.digibooky.dto;
+
+import com.switchfully.digibooky.domain.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    private UserDTO mapToDTO(User user) {
+        return null;
+    }
+}
