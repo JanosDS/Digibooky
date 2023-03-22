8e5c5a8fce91c3d3d9bdf46eb7ebf7991d23c145
Jimmy-Severins <123968582+Jimmy-Severins@users.noreply.github.com>
2023-03-22 09:48:10 +0100

    syntax update

--- src/main/java/com/switchfully/digibooky/repository/AddressRepository.java
+++ src/main/java/com/switchfully/digibooky/repository/AddressRepository.java
@@ -2,11 +2,12 @@ package com.switchfully.digibooky.repository;
 
 import com.switchfully.digibooky.domain.Address;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Repository
 public class AddressRepository {
     private static List<Address> addressList;
 }
