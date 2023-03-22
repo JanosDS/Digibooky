82b0918604de4a100ef3bb7dc0f4eaa148240c1f
Wouter Sels <phaerocode@hotmail.com>
2023-03-22 09:45:20 +0100

    story 0: changed rentalController annotation

--- src/main/java/com/switchfully/digibooky/api/RentalController.java
+++ src/main/java/com/switchfully/digibooky/api/RentalController.java
@@ -2,9 +2,11 @@ package com.switchfully.digibooky.api;
 
 import com.switchfully.digibooky.service.RentalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "rental")
+@RestController()
+@RequestMapping("/rentals")
 public class RentalController {
 
     private RentalService rentalService;
