c49fd7c10c57e318f46fd23fe2ceeb9b939f2318
Janos <janosdescheemaeker@hotmail.com>
2023-03-23 11:01:26 +0100

    Story 7: poor mans security added

--- src/main/java/com/switchfully/digibooky/repository/UserRepository.java
+++ src/main/java/com/switchfully/digibooky/repository/UserRepository.java
@@ -6,6 +6,7 @@ import com.switchfully.digibooky.domain.user.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Repository
 public class UserRepository {
@@ -14,8 +15,10 @@ public class UserRepository {
         this.userList = new ArrayList<>();
 
         //Add user for testing purposes
-        userList.add(new User("Janos", "Descheemaeker", "janosdescheemaeker@hotmail.com", new Address("Leiestraat", "29", "9000", "Gent", "Belgium"), "MyINSS", Role.MEMBER));
-    }
+        userList.add(new User("Janos", "Descheemaeker", "janosdescheemaeker@hotmail.com", new Address("Leiestraat", "29", "9000", "Gent", "Belgium"), "MyINSS", Role.MEMBER, "123"));
+		//Add an Admin for testing purposes
+		userList.add(new User("Admin", "Admin", "admin@admin.be", new Address("Adminstreet", "1", "9001", "AdminCity", "Belgium"), "AdminINSS", Role.ADMIN, "admin"));
+	}
 
     public List<User> getUserList() {
         return userList;
@@ -30,5 +33,17 @@ public class UserRepository {
 		return userList.stream()
 				.filter(user -> user.getInss().equals(INSS))
 				.findFirst();
+	}
+
+	public Optional<User> findUserForUsername(String username) {
+		return userList.stream()
+				.filter(user -> user.getFirstName().equals(username))
+				.findFirst();
+	}
+
+	public List<User> getAllMembers(){
+		return userList.stream()
+				.filter(user -> user.getRole().equals(Role.MEMBER))
+				.collect(Collectors.toList());
 	}
 }
