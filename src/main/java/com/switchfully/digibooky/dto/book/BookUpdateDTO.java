bd434f279d93455d7d4af0163cd4b9cf302e1776
gulsumgucenmez <86490486+gulsumgucenmez@users.noreply.github.com>
2023-03-23 13:49:20 +0100

    refactored namings

--- src/main/java/com/switchfully/digibooky/dto/book/BookUpdateDTO.java
+++ src/main/java/com/switchfully/digibooky/dto/book/BookUpdateDTO.java
@@ -1,24 +1,23 @@
 package com.switchfully.digibooky.dto.book;
 
-import com.switchfully.digibooky.domain.Author;
 import com.switchfully.digibooky.dto.author.AuthorDTO;
 
 import java.util.List;
+
 public class BookUpdateDTO {
 
-    private final String ISBN;
+    private final String isbn;
     private final String title;
     private final List<AuthorDTO> authorList;
-
     private final String summary;
-    private boolean isAvailable;
+    private final boolean isAvailable;
 
-public BookUpdateDTO(String ISBN,String title, List<AuthorDTO> authorList, String summary, boolean isAvailable) {
+    public BookUpdateDTO(String isbn, String title, List<AuthorDTO> authorList, String summary, boolean isAvailable) {
         this.title = title;
         this.authorList = authorList;
         this.summary = summary;
         this.isAvailable = isAvailable;
-        this.ISBN = ISBN;
+        this.isbn = isbn;
     }
 
     public String getTitle() {
@@ -36,8 +35,7 @@ public BookUpdateDTO(String ISBN,String title, List<AuthorDTO> authorList, Strin
     public boolean isAvailable() {
         return isAvailable;
     }
-
-    public String getISBN() {
-        return ISBN;
+    public String getIsbn() {
+        return isbn;
     }
 }
