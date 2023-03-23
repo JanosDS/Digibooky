cd01cd42f23c7156e725b17944cb98eeb958cd4d
gulsumgucenmez <86490486+gulsumgucenmez@users.noreply.github.com>
2023-03-23 13:44:33 +0100

    refactored: deleted (unused setters)

--- src/main/java/com/switchfully/digibooky/dto/book/BookDTO.java
+++ src/main/java/com/switchfully/digibooky/dto/book/BookDTO.java
@@ -1,55 +1,40 @@
 package com.switchfully.digibooky.dto.book;
+
 import com.switchfully.digibooky.dto.author.AuthorDTO;
 
 import java.util.List;
 
 public class BookDTO {
-	private final String isbn;
-	private  String title;
-	private  List<AuthorDTO> authorList;
+    private final String isbn;
+    private String title;
+    private List<AuthorDTO> authorList;
+    private String summary;
+    private boolean isAvailable;
 
-	private  String summary;
-	private boolean isAvailable;
+    public BookDTO(String isbn, String title, List<AuthorDTO> authorList, String summary, boolean isAvailable) {
+        this.isbn = isbn;
+        this.title = title;
+        this.authorList = authorList;
+        this.summary = summary;
+        this.isAvailable = isAvailable;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
 
-	public BookDTO(String isbn, String title, List<AuthorDTO> authorList, String summary, boolean isAvailable) {
-		this.isbn = isbn;
-		this.title = title;
-		this.authorList = authorList;
-		this.summary = summary;
-		this.isAvailable = isAvailable;
-	}
-	public String getIsbn() {
-		return isbn;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getSummary() {
+        return summary;
+    }
 
-	public String getSummary() {
-		return summary;
-	}
-
-	public boolean isAvailable() {
-		return isAvailable;
-	}
-
-	public List<AuthorDTO> getAuthorList() {
-		return authorList;
-	}
-
-	public void setAvailable(boolean available) {
-		isAvailable = available;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public void setSummary(String summary) {
-		this.summary = summary;
-	}
-	public void setAuthorList(List<AuthorDTO> authorList) {
-		this.authorList = authorList;
-	}
-
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    public List<AuthorDTO> getAuthorList() {
+        return authorList;
+    }
 
 }
